407e7ae560a2e87bd3c4424ee479ef85e768c6f0
Louis469 <louismoeykent@outlook.com>
2020-05-28 17:14:15 +0930

    Keep on coding!

--- client/bio.js
+++ client/bio.js
@@ -1,1 +1,6 @@
-let name = prompt("Whats your name???");
+const name = prompt("Whats your name?");
+const birth_date = prompt("Whats your birth date? (in format like 8/8/20)");
+
+let notes = document.createElement("p");
+notes.textContent = name + birth_date;
+document.body.appendChild(notes);
